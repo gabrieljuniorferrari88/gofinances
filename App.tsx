57af5ceb44936f8e18f035ddcbd4a43d37f55234
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-01-12 11:04:20 -0300

    Definindo as routas e instalando a navegacao do app

--- App.tsx
+++ App.tsx
@@ -9,7 +9,9 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
+
 import AppLoading from 'expo-app-loading';
 
 export default function App() {
@@ -25,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
